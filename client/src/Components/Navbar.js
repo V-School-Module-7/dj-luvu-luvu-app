d3ae1f2abdf650ea5d88241d2fecbf79fd94b982
Jax Clark <jamaecla@gmail.com>
2020-01-23 13:21:11 -0700

    fixed links in navbar and responsiveness in soundcloud component

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import logoDanOrange from '../Images/DJ-Luva-Luva_Dan-orange.png'
-import { Link, animateScroll as scroll } from 'react-scroll'
+import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll'
 
 export default function Navbar() {
 
@@ -10,24 +10,24 @@ export default function Navbar() {
             <Link exact to='/' >
                 <img src={logoDanOrange} alt='DJ Luva Luva Logo' className='navbarLogo' />
             </Link>
-            <Link to='/booking' className='navLinks'>BOOKING</Link>
-            <Link
+            <Link to='/booking' className='navLinks1'>BOOKING</Link>
+            <ScrollLink
                 activeClass='active'
                 to='soundcloud'
                 spy={true}
                 smooth={true}
                 offset={-50}
                 duration={500}
-                className='navLinks'>MUSIC</Link>
-            <Link to='/contact' className='navLinks'>CONTACT</Link>
-            <Link 
+                className='navLinks2'>MUSIC</ScrollLink>
+            <Link to='/contact' className='navLinks1'>CONTACT</Link>
+            <ScrollLink 
                 activeClass='active'
                 to='about'
                 spy={true}
                 smooth={true}
                 offset={-50}
                 duration={500}
-                className='navLinks'>ABOUT</Link>
+                className='navLinks2'>ABOUT</ScrollLink>
         </div>
     )
 }
--- client/src/Components/Soundcloud.js
+++ client/src/Components/Soundcloud.js
@@ -20,49 +20,49 @@ export default function Soundcloud() {
                     <hr className='songSpacer' />
                     <div onClick={() => handleChange('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/710858575&color=%23ff9533&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true')} className='songDiv'>
                         <img src="https://i1.sndcdn.com/artworks-000635229793-t2tz6m-t500x500.jpg" alt='Song 1 Image'/>
-                        <p className='songTitle'>Rich Brian X Chungha - These Nights (Luva X Prinz) Remix</p>
+                        <div className='songTitle'>Rich Brian X Chungha - These Nights (Luva X Prinz) Remix</div>
                     </div>
                     {/* two */}
                     <hr className='songSpacer' />
                     <div className='songDiv' onClick={() => handleChange("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/644740053&color=%23ff9533&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true")}>
                         <img src="https://i1.sndcdn.com/artworks-000560087532-6vh1ah-t500x500.jpg" alt="Song 2 Image"/>
-                        <p className='songTitle'>T - Pain - F.B.G.M. Vs S.O.S. Luva Luva Edit</p>
+                        <div className='songTitle'>T - Pain - F.B.G.M. Vs S.O.S. Luva Luva Edit</div>
                     </div>
                     {/* three */}
                     <hr className='songSpacer' />
                     <div className='songDiv' onClick={() => handleChange("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/233071565&color=%23ff9533&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true")}>
                         <img src="https://i1.sndcdn.com/artworks-000136154039-r1b84h-t500x500.jpg" alt="Song 3 Image"/>
-                        <p className='songTitle'>Andru Markel - 2025 (Luva & J-Pan Bootleg)</p>
+                        <div className='songTitle'>Andru Markel - 2025 (Luva & J-Pan Bootleg)</div>
                     </div>
                     {/* four */}
                     <hr className='songSpacer' />
                     <div className='songDiv' onClick={() => handleChange("https://www.mixcloud.com/widget/iframe/?feed=%2Fdjluvaluva%2Fcorporate-mix%2F")}>
                         <img src="https://i1.sndcdn.com/artworks-000560087532-6vh1ah-t500x500.jpg" alt="Song 4 Image"/>
-                        <p className='songTitle'>Corporate Mix</p>
+                        <div className='songTitle'>Corporate Mix</div>
                     </div>
                     {/* five */}
                     <hr className='songSpacer' />
                     <div className='songDiv' onClick={() => handleChange("https://www.mixcloud.com/widget/iframe/?feed=%2Fdjluvaluva%2Fdance-mix-vol-2%2F")}>
                         <img src="https://i1.sndcdn.com/artworks-000560087532-6vh1ah-t500x500.jpg" alt="Song 5 Image"/>
-                        <p className='songTitle'>Dance Mix Vol. 2</p>
+                        <div className='songTitle'>Dance Mix Vol. 2</div>
                     </div>
                     {/* six */}
                     <hr className='songSpacer' />
                     <div className='songDiv' onClick={() => handleChange("https://www.mixcloud.com/widget/iframe/?feed=%2Fdjluvaluva%2Fjanuary-2019-dance-mix%2F")}>
                         <img src="https://i1.sndcdn.com/artworks-000560087532-6vh1ah-t500x500.jpg" alt="Song 6 Image"/>
-                        <p className='songTitle'>Dance Mix Vol. 1</p>
+                        <div className='songTitle'>Dance Mix Vol. 1</div>
                     </div>
                     {/* seven */}
                     <hr className='songSpacer' />
                     <div className='songDiv' onClick={() => handleChange("https://www.mixcloud.com/widget/iframe/?feed=%2Fdjluvaluva%2Flate-night-slc-vol-1%2F")}>
                         <img src="https://i1.sndcdn.com/artworks-000560087532-6vh1ah-t500x500.jpg" alt="Song 7 Image"/>
-                        <p className='songTitle'>Late Night SLC Vol .1</p>
+                        <div className='songTitle'>Late Night SLC Vol .1</div>
                     </div>
                     {/* eight */}
                     <hr className='songSpacer' />
                     <div className='songDiv' onClick={() => handleChange("https://www.mixcloud.com/widget/iframe/?feed=%2Fdjluvaluva%2Fsummer-time-energy%2F")}>
                         <img src="https://i1.sndcdn.com/artworks-000560087532-6vh1ah-t500x500.jpg" alt="Song 8 Image"/>
-                        <p className='songTitle'>Summer Time Energy</p>
+                        <div className='songTitle'>Summer Time Energy</div>
                     </div>
                 </div>
         </div>
