2a7b24716d9a19b3691a4058fd9bf4294456fe37
Jax Clark <jamaecla@gmail.com>
2020-01-23 18:46:17 -0700

    fixed navbar links and styling on soundcloud component and contact forms

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,9 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useLocation} from 'react'
+import { Link, withRouter } from 'react-router-dom'
 import logoDanOrange from '../Images/DJ-Luva-Luva_Dan-orange.png'
 import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll'
 
-export default function Navbar() {
+function Navbar(props) {
+    const path = props.history.location.pathname
+
+    // const testPush = () => {
+    //     this.props.history.push('/').then(
+    //         window.scrollTo(0, 0)
+    //     )
+    // }
 
     return(
         <div className='nav'>
@@ -11,23 +18,37 @@ export default function Navbar() {
                 <img src={logoDanOrange} alt='DJ Luva Luva Logo' className='navbarLogo' />
             </Link>
             <Link to='/booking' className='navLinks1'>BOOKING</Link>
-            <ScrollLink
-                activeClass='active'
-                to='soundcloud'
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={500}
-                className='navLinks2'>MUSIC</ScrollLink>
+            { path === '/' ?
+                <ScrollLink
+                    activeClass='active'
+                    to='soundcloud'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className='navLinks2'>
+                    MUSIC
+                </ScrollLink>
+                :
+                <Link to='/' className='navLinks2' >MUSIC</Link>
+            }
             <Link to='/contact' className='navLinks1'>CONTACT</Link>
-            <ScrollLink 
-                activeClass='active'
-                to='about'
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={500}
-                className='navLinks2'>ABOUT</ScrollLink>
+            { path === '/' ?
+                <ScrollLink
+                    activeClass='active'
+                    to='about'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className='navLinks2'>
+                    ABOUT
+                </ScrollLink>
+                :
+                <Link to='/' className='navLinks2' >ABOUT</Link>
+            }
         </div>
     )
 }
+
+export default withRouter(Navbar)
