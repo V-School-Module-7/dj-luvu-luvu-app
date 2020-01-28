454542a2b642072edf1b43aef0e6a46fa5e6e0f9
Betsy Popek <betsypopek@gmail.com>
2020-01-28 15:31:38 -0700

    scroll testing

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { withRouter } from 'react-router-dom'
 import Soundcloud from './Soundcloud'
 import UpcomingShows from './UpcomingShows'
 import About from './About'
 import Social from './Social'
-import LuvaHomeImage from '../Images/Luva-1c.jpg'
+// import LuvaHomeImage from '../Images/Luva-1c.jpg'
 import LuvaHomeImage2 from '../Images/Luva-14-c.jpg'
+var Scroll = require('react-scroll')
+var scroller = Scroll.scroller;
 
 
-export default function Home() {
+function Home(props) {
     // useEffect(() => {
     //     document.documentElement.scrollTop = (scrollVariable)
     // })
+
+    // useEffect(() => {
+    //     scroller.scrollTo(props.location.state.section)
+    // })
+    // console.log(props.location)
     
     return(
         <div className='homeContainer'>
@@ -22,3 +30,5 @@ export default function Home() {
         </div>
     )
 }
+
+export default withRouter(Home)
