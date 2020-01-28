3647d4b346e065410d89c3f23d22fb451ac70ae0
Betsy Popek <betsypopek@gmail.com>
2020-01-28 12:30:19 -0700

    changed image and added video

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -4,11 +4,13 @@ import UpcomingShows from './UpcomingShows'
 import About from './About'
 import Social from './Social'
 import LuvaHomeImage from '../Images/Luva-1c.jpg'
+import LuvaHomeImage2 from '../Images/Luva-14-c.jpg'
+
 
 export default function Home() {
     return(
         <div className='homeContainer'>
-            <img src={LuvaHomeImage} alt='DJ Luva Luva' className='homeImage'/>
+            <img src={LuvaHomeImage2} alt='DJ Luva Luva' className='homeImage'/>
             <Soundcloud className='soundCloudContainer' id='soundcloud'/>
             <UpcomingShows className='upcomingShowsContainer' id='upcomingShows'/>
             <About className='aboutContainer' id='about'/>
