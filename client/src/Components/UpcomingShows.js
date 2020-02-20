0b8d00b9d4287164908be9de9d3cf4993f9623f1
Betsy Popek <betsypopek@gmail.com>
2020-02-20 14:05:45 -0700

    fixed home image/color

--- client/src/Components/UpcomingShows.js
+++ client/src/Components/UpcomingShows.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 // import SCPlaceholder from '../Images/Luva-14.jpg'
 import ShowsList from './ShowsList'
+import { ShowContext } from './Provider'
+
 
 export default function UpcomingShows() {   
+
+    useEffect(() => {
+        getShows()
+
+    }, [])
+
+    const { shows, getShows } = useContext(ShowContext)
+    console.log(shows)
     return(
-        <div className='upcomingShows'>
+        <div className={ shows[0] ? 
+            'upcomingShowsColor'
+            :
+            'upcomingShows'}>
             <ShowsList type='upcomingShows'/>
         </div>
     )
